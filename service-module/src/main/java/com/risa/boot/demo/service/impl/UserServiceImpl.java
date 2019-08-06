b0bf41c79fb79732f08de5c4f1c28e278e79ebee
Artiron <kurdina91@mail.ru>
2019-08-06 21:26:27 +0400

    tests

---
+++ service-module/src/main/java/com/risa/boot/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package com.risa.boot.demo.service.impl;
+
+
+import com.risa.boot.demo.entity.User;
+import com.risa.boot.demo.repository.RoleRepository;
+import com.risa.boot.demo.repository.UserRepository;
+import com.risa.boot.demo.service.api.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(new HashSet<>(roleRepository.findAll()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
